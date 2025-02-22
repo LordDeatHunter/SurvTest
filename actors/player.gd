7ccb44bc8929e0ab7ae3e5576893513bc6378bc0
LordDeatHunter <desuhansa@gmail.com>
2025-02-23 00:00:45 +0100

    Update player movement

--- actors/player.gd
+++ actors/player.gd
@@ -1,70 +1,79 @@
+class_name Player
 extends CharacterBody3D
 
-const WALK_SPEED = 5.0
-const SPRINT_SPEED = 5.0
-const JUMP_VELOCITY = 4.5
-const SENSITIVITY = 0.01
+const WALK_SPEED: float = 5.0
+const SPRINT_SPEED: float = 7.5
+const JUMP_VELOCITY: float = 4.5
+const SENSITIVITY: float = 0.004
+const BOB_FREQ: float = 0.25
+const BOB_AMP: float = 0.1
+const BASE_FOV: int = 75
+const WALK_FOV_MULT: float = 1.3
+const SPRINT_FOV_MULT: float = 2.6
 
-const BOB_FREQ = 2.0
-const BOB_AMP = 0.08
-var t_bob = 0
-
-const BASE_FOV = 75
-const FOV_CHANGE = 2.5
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
+var t_bob: int = 0
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 @onready var head = $Head
 @onready var camera = $Head/Camera3D
 
+
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	
+
+
 func _unhandled_input(event):
 	if event is InputEventMouseMotion:
 		head.rotate_y(-event.relative.x * SENSITIVITY)
 		camera.rotate_x(-event.relative.y * SENSITIVITY)
-		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-40), deg_to_rad(60))
+		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-90), deg_to_rad(90))
+
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y -= gravity * delta
+	velocity -= Vector3(0, gravity, 0) * delta
 
-	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		
-	var speed = SPRINT_SPEED if Input.is_action_pressed("sprint") else WALK_SPEED
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
+	var is_sprinting: bool = Input.is_action_pressed("sprint")
+	var speed: float = SPRINT_SPEED if is_sprinting else WALK_SPEED
+
+	var input_dir: Vector2 = Input.get_vector(
+		"move_left", "move_right", "move_forward", "move_backward"
+	)
 	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	
-	if is_on_floor():
-		if direction:
+
+	if direction:
+		if is_on_floor():
 			velocity.x = direction.x * speed
 			velocity.z = direction.z * speed
 		else:
-			velocity.x = lerp(velocity.x, direction.x * speed, delta * 7.0)
-			velocity.z = lerp(velocity.x, direction.x * speed, delta * 7.0)
+			velocity.x = lerp(velocity.x, direction.x * speed, delta * 10)
+			velocity.z = lerp(velocity.z, direction.z * speed, delta * 10)
 	else:
-		velocity.x = lerp(velocity.x, direction.x * speed, delta * 3.0)
-		velocity.z = lerp(velocity.x, direction.x * speed, delta * 3.0)
-	
-	t_bob += delta * velocity.length() * float(is_on_floor())
+		velocity.x = lerpf(velocity.x, 0, delta * 10)
+		velocity.z = lerpf(velocity.z, 0, delta * 10)
+
+	t_bob += velocity.length() * float(is_on_floor()) * delta * 10
 	camera.transform.origin = _headbob(t_bob)
-	
-	var velocity_clamped = clamp(velocity.length(), 0.5, SPRINT_SPEED * 2)
-	var target_fov = BASE_FOV + FOV_CHANGE * velocity_clamped
+
+	var xz_velocity: Vector3 = Vector3(velocity.x, 0, velocity.z)
+
+	var velocity_clamped = clamp(xz_velocity.length(), 0.5, SPRINT_SPEED * 2)
+	var target_fov = (
+		BASE_FOV + (SPRINT_FOV_MULT if is_sprinting else WALK_FOV_MULT) * velocity_clamped
+	)
 	camera.fov = lerp(camera.fov, target_fov, delta * 8.0)
 
 	move_and_slide()
 
+
 func _headbob(time) -> Vector3:
-	var pos = Vector3.ZERO
+	var pos: Vector3 = Vector3.ZERO
 	pos.y = sin(t_bob * BOB_FREQ) * BOB_AMP
 	pos.x = cos(time * BOB_FREQ / 2) * BOB_AMP
 	return pos
+
+
+func get_height() -> float:
+	return head.transform.origin.y
---
+++ scenes/main.gd
@@ -0,0 +1,10 @@
+extends Node3D
+
+
+func _ready():
+	pass
+
+
+func _process(_delta):
+	if Input.is_action_just_pressed("exit"):
+		get_tree().quit()

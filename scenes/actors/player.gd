96c21eab9fa50069891b3c41ada0e64497a762ab
LordDeatHunter <desuhansa@gmail.com>
2025-06-22 18:31:44 +0200

    Add crouching

--- scenes/actors/player.gd
+++ scenes/actors/player.gd
@@ -10,6 +10,11 @@ const BOB_AMP: float = 0.1
 const BASE_FOV: int = 75
 const WALK_FOV_MULT: float = 1.3
 const SPRINT_FOV_MULT: float = 2.6
+const DEFAULT_HEIGHT: float = 2.0
+const CROUCH_HEIGHT: float = 1.5
+const CAMERA_HEIGHT: float = 1.75
+const CAMERA_CROUCH_HEIGHT: float = 1.25
+
 @export var max_multijumps: int = 0
 
 var t_bob: int = 0
@@ -18,9 +23,23 @@ var current_multijumps: int = 0
 var is_sprinting: bool = false
 var time_since_last_forward: float = 1.0
 var max_sprint_press_delay: float = 0.5
+var height: float = 2.0
+var collision_capsule_shape: CapsuleShape3D:
+	get:
+		if not collision_shape_3d.shape:
+			return null
+		return collision_shape_3d.shape as CapsuleShape3D
+var capsule_mesh: CapsuleMesh:
+	get:
+		if not mesh_instance_3d.mesh:
+			return null
+		return mesh_instance_3d.mesh as CapsuleMesh
+var is_crouching: bool = false
 
 @onready var head = $Head
 @onready var camera = $Head/Camera3D
+@onready var mesh_instance_3d: MeshInstance3D = $MeshInstance3D
+@onready var collision_shape_3d: CollisionShape3D = $CollisionShape3D
 
 
 func _ready():
@@ -38,6 +57,7 @@ func _physics_process(delta):
 	velocity -= Vector3(0, gravity, 0) * delta
 
 	_handle_jumping()
+	_handle_crouching(delta)
 	_handle_sprinting(delta)
 
 	var speed: float = SPRINT_SPEED if is_sprinting else WALK_SPEED
@@ -100,7 +120,7 @@ func _handle_sprinting(delta: float):
 	else:
 		time_since_last_forward += delta
 
-	if not Input.is_action_pressed("move_forward"):
+	if not Input.is_action_pressed("move_forward") or is_crouching:
 		is_sprinting = false
 
 
@@ -114,3 +134,30 @@ func _handle_jumping():
 		velocity.y = JUMP_VELOCITY
 		if not is_on_floor():
 			current_multijumps += 1
+
+
+func _handle_crouching(delta: float):
+	if Input.is_action_just_pressed("crouch"):
+		is_crouching = true
+	if Input.is_action_just_released("crouch"):
+		is_crouching = false
+
+	if not is_on_floor():
+		is_crouching = false
+
+	var lerp_amount: float = 10 * delta
+
+	if is_crouching:
+		height = CROUCH_HEIGHT
+		head.transform.origin.y = lerp(head.transform.origin.y, CAMERA_CROUCH_HEIGHT, lerp_amount)
+	else:
+		height = DEFAULT_HEIGHT
+		head.transform.origin.y = lerp(head.transform.origin.y, CAMERA_HEIGHT, lerp_amount)
+
+	if collision_capsule_shape:
+		collision_capsule_shape.height = lerp(collision_capsule_shape.height, height, lerp_amount)
+		collision_shape_3d.position.y = lerp(collision_shape_3d.position.y, height / 2, lerp_amount)
+
+	if capsule_mesh:
+		capsule_mesh.height = lerp(capsule_mesh.height, height, lerp_amount)
+		mesh_instance_3d.position.y = lerp(mesh_instance_3d.position.y, height / 2, lerp_amount)

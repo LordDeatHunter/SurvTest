c250a22a8b302614b5f8adc0b3ed39c99a6ffc85
LordDeatHunter <desuhansa@gmail.com>
2025-07-06 00:28:02 +0200

    Spawn items on game start

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,9 +1,10 @@
 extends Node3D
 
+const DROPPED_ITEM_SCENE: PackedScene = preload("res://scenes/DroppedItem.tscn")
+const GRASS_PATCH_SCENE: PackedScene = preload("res://scenes/grass_patch.tscn")
+
 @export var ground_size: Vector2 = Vector2(32, 32)
 @export var grass_cull_distance: float = 150
-
-var grass_patch_scene: PackedScene = preload("res://scenes/grass_patch.tscn")
 
 @onready var player: Player = $Player
 @onready var grass_patches: Node = $GrassPatches
@@ -12,9 +13,14 @@ var grass_patch_scene: PackedScene = preload("res://scenes/grass_patch.tscn")
 func _ready():
 	for x in range(-ground_size.x / 2, ground_size.x / 2 + 1):
 		for z in range(-ground_size.y / 2, ground_size.y / 2 + 1):
-			var grass_patch: StaticBody3D = grass_patch_scene.instantiate()
+			var grass_patch: StaticBody3D = GRASS_PATCH_SCENE.instantiate()
 			grass_patch.position = Vector3(x * 20, 0, z * 20)
 			grass_patches.add_child(grass_patch)
+
+	for i in range(16):
+		var dropped_item: DroppedItem = DROPPED_ITEM_SCENE.instantiate()
+		dropped_item.setup(Items.create_example_item(), player.position + Vector3(0, 2, 0))
+		add_child(dropped_item)
 
 	_on_hide_patches_timeout()
 

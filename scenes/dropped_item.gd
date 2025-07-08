07ef7e3819f4636c9f98b74d4c1991206158289a
LordDeatHunter <desuhansa@gmail.com>
2025-07-09 01:21:08 +0200

    Create separate highlight color instance per dropped item

--- scenes/dropped_item.gd
+++ scenes/dropped_item.gd
@@ -3,6 +3,7 @@ extends RigidBody3D
 
 var stack: ItemStack = ItemStack.new(null, 0)
 var size_tween: Tween
+var material_overlay: StandardMaterial3D
 
 @onready var mesh_instance: MeshInstance3D = $MeshInstance3D
 
@@ -11,6 +12,10 @@ func _ready() -> void:
 	size_tween = create_tween().set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT).set_loops()
 	size_tween.tween_property(mesh_instance, "scale", Vector3.ONE * 1.2, 0.75)
 	size_tween.tween_property(mesh_instance, "scale", Vector3.ONE * 0.8, 0.75)
+
+	material_overlay = StandardMaterial3D.new()
+	mesh_instance.material_overlay = material_overlay
+	set_highlighted(false)
 
 
 func setup(item_stack: ItemStack, spawn_position: Vector3 = Vector3.ZERO) -> void:
@@ -27,13 +32,7 @@ func try_pick_up(inventory: Inventory) -> bool:
 
 
 func set_highlighted(is_highlighted: bool) -> void:
-	var material: StandardMaterial3D = _get_standard_material()
 	if is_highlighted:
-		material.albedo_color = Color(0, 1.0, 0.55, 0.5)
+		material_overlay.albedo_color = Color(0, 1.0, 0.55, 0.5)
 	else:
-		material.albedo_color = Color(0, 0.8, 0.35)
-
-
-func _get_standard_material() -> StandardMaterial3D:
-	var mesh: BoxMesh = mesh_instance.mesh as BoxMesh
-	return mesh.material as StandardMaterial3D if mesh else null
+		material_overlay.albedo_color = Color(0, 0.8, 0.35)

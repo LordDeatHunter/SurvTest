d023036e01ae61a6f62c473ac1b2d74bf18aa6df
LordDeatHunter <desuhansa@gmail.com>
2025-09-07 13:35:20 +0200

    Adjust sword hitbox

--- scenes/items/sword.gd
+++ scenes/items/sword.gd
@@ -3,11 +3,11 @@ extends Node3D
 
 @onready var animation_player: AnimationPlayer = %AnimationPlayer
 
+
 func swing() -> void:
-	animation_player.play('attack')
+	animation_player.play("attack")
 
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
-	print(body)
 	if body is RigidBody3D:
 		body.apply_force(Vector3(0, 200, 0))
